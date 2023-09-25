0a92238e0b3fe61057d8bfed63999d3d2a2264c2
PieroLa28 <hilter.lanche@upeu.edu.pe>
2023-09-24 21:25:12 -0500

    modifique estilos

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [LoginPagesComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, InputTextModule, PasswordModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
 })
 export class AuthModule {}
